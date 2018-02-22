90edc1157a4a58c74eaf5a1796631f9e10a64d37
Uday Kumar <ukb0002@uah.edu>
2018-02-22 10:54:27 -0600

    Added Class in Index.js

--- src/App.js
+++ src/App.js
@@ -4,3 +4,12 @@ const log = val => console.log(val);
 const App = () => <h1>Hello World!!</h1>;
 
 const filterVal = [1,2,3,4,5].filter(val=> val<4);
+
+class Person {
+    constructor(){
+
+    }
+    getFullName(){
+        return "Uday Kumar Billur";
+    }
+}
